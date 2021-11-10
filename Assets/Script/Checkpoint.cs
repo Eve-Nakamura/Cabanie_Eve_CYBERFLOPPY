36230a8eaba465494cfb580f503eba102ad742b2
lydcat <evacabana@live.fr>
2021-11-10 17:13:05 +0100

    0.5

--- Assets/Script/Checkpoint.cs
+++ Assets/Script/Checkpoint.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class Checkpoint : MonoBehaviour //à chaque fois que le player passe dessus, le score augmente de 1
 {
     public float Speed;
-    public Vector3 initialPosition;
-    // Update is called once per frame
+    public Vector3 initialPosition; //leur pose de départ
+    // on crée le système de spon des checkpoint
     void Start()
     {
-        transform.position -= Vector3.right * Speed;
+        transform.position -= Vector3.right * Speed; //on les fait bouger dès le départ en même temps que les obstacles
 
     }
 
     void Update()
     {
-        transform.position -= Vector3.right * Speed;
+        transform.position -= Vector3.right * Speed; //on répète l'ordre à chaque frame
     }
     private void OnTriggerEnter2D(Collider2D p_collision)
     {
        
         if (p_collision.GetComponent<Player>() != null)
         {
-            FindObjectOfType<ScoreSystem>().addScore();
+            FindObjectOfType<ScoreSystem>().addScore(); //on leur met une collision pour update le score
         }
 
 
@@ -30,6 +30,6 @@ public class Checkpoint : MonoBehaviour
 
     public void relaunchObject()
     {
-        transform.position = initialPosition;
+        transform.position = initialPosition; //on crée la fct pour que les checkpoints reviennent à l'infini
     }
 }
